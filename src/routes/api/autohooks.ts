fca3dad106559d4175e6ace55efd9fba9007ab18
donywan <donywan@outlook.com>
2025-02-11 12:18:08 +0800

    add api hook

--- src/routes/api/autohooks.ts
+++ src/routes/api/autohooks.ts
@@ -1,1 +1,8 @@
+import { FastifyInstance } from "fastify";
+
 // Use bearer-token plugin to verify token
+export default async function (fastify: FastifyInstance, options: any) {
+    // 添加请求钩子
+    fastify.addHook('onRequest', async (request, reply) => {
+    })
+}
